68cd979b4f954e121a234bc95de753f11009554b
Haci Yaman <haci.yaman@xplora.com>
2022-12-15 16:30:18 +0000

    minor update

--- src/factory.ts
+++ src/factory.ts
@@ -1,31 +1,52 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
 import { newMessagePublisher } from './message-publishers';
 
 export async function factory(penv = process.env) {
   const app = express();
 
-  const msgPublisher = await newMessagePublisher({
-    kind: penv.MP_KIND || 'rabbitmq',
-    conf: {
-      hostname : penv.MP_HOSTNAME || 'localhost',
-      port     : Number.parseInt(penv.MP_PORT || '0'),
-      username : penv.MP_USERNAME || '',
-      password : penv.MP_PASSWORD || '',
-      heartbeat: 30,
-    },
-  });
+  // TODO: security by API Key?
 
-  app.post('/:queue', async (req, res) => {
+  app.use(express.text({ limit: '1MB' }));
+  app.use(express.json({ limit: '1MB' }));
+
+  const config = {
+    http: {
+      port: Number.parseInt(penv.MB_HTTP_PORT || '3000'),
+    },
+    messagePublisher: {
+      kind: penv.MP_KIND || 'rabbitmq',
+      conf: {
+        hostname : penv.MP_HOSTNAME || 'localhost',
+        port     : Number.parseInt(penv.MP_PORT || '0'),
+        username : penv.MP_USERNAME || '',
+        password : penv.MP_PASSWORD || '',
+        heartbeat: 30,
+      },
+    },
+  };
+
+  const msgPublisher = await newMessagePublisher(config.messagePublisher);
+
+  async function healthCheck(_req: Request, res: Response) {
+    res.json({ status: 'OK', ts: new Date() });
+  }
+
+  app.get('/health', healthCheck);
+
+  async function handleQueue(req: Request, res: Response) {
     try {
-      const queue   = String(req.params.queue || '');
-      const payload = String(req.body);
-      const result  = await msgPublisher.publish({ queue, payload });
+      const { queue } = req.params as Record<string, string>;
+      // TODO: validate req.body based on the contract for that queue
+      const payload  = JSON.stringify(req.body);
+      const result   = await msgPublisher.publish({ queue, payload });
       res.json(result);
     } catch (err) {
       console.error('message-publisher error', err);
       res.json({ error: 'Server error' });
     }
-  });
+  }
 
-  return { app, msgPublisher };
+  app.post('/:queue', handleQueue);
+
+  return { app, config, msgPublisher, healthCheck, handleQueue };
 }
